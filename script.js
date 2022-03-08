b157019a94a595650936251f0369e5e762b6092b
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-03-08 07:05:27 -0800

    sticky nav bar feature added

--- script.js
+++ script.js
@@ -2,8 +2,11 @@
 
 // HTML Elements
 
+const navBar = document.querySelector('.nav-bar');
 const navLinksEl = document.querySelectorAll(".nav_link");
 const navMenuEl = document.querySelector('.nav-menu');
+const heroSection = document.getElementById('hero');
+console.log(navBar);
 
 // Smooth scrolling using event delegation 
 navMenuEl.addEventListener('click', function(e) {
@@ -12,7 +15,35 @@ navMenuEl.addEventListener('click', function(e) {
     const sectionScrollTo = document.querySelector(e.target.getAttribute('href'));
     sectionScrollTo.scrollIntoView({behavior: 'smooth'});
   }
-})
+});
+
+// Sticky nav using the Intersection observer API
+
+const stickyNav = function(entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) {
+    navBar.classList.add('sticky-nav');
+  } else {
+    navBar.classList.remove('sticky-nav');
+  }
+  ovserver.unobserve(navBar);
+}
+
+const ovserver = new IntersectionObserver(stickyNav, {
+  root: null, 
+  threshold: 0.10,
+  rootMargin: '-50px'
+});
+ovserver.observe(heroSection);
+
+
+
+
+
+
+
+
 
 /*
 
